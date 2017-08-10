c28689bf72b6f1d33dac796351a0298462be5fdc
Suguru Kouchi <e155742@ie.u-ryukyu.ac.jp>
2017-08-10 20:22:34 +0900

    calculation error check
    2乗後の行列の左上もしくは右下が0の場合、計算が正しく行われなかったとみてエラーを返す。
    ついでに、スレッド数*ブロック数が行列サイズの2乗を下回らないように修正。

--- Makefile
+++ Makefile
@@ -10,16 +10,18 @@ OBJ = $(SRC:.cu=.o)
 PROGCPU = cpu
 PROGCUDA = cuda
 
-all: clean cpu cuda
+all: cleanobj cpu cuda
 
-cpu: clean $(SRC)
+cpu: cleanobj $(SRC)
 	$(CC) $(CFLAGS) -x c -c -o $(OBJ) $(SRC)
 	$(CC) $(CFLAGS) -o $(PROGCPU) $(OBJ)
 
-cuda gpu: clean $(SRC)
+cuda gpu: cleanobj $(SRC)
 	$(CUC) $(CUFLAGS) -c -o $(OBJ) $(SRC)
 	$(CUC) $(CUFLAGS) -o $(PROGCUDA) $(OBJ)
 
-clean:
-	rm -f $(OBJ) $(PROGCPU) $(PROGCUDA)
+clean: cleanobj
+	rm -f $(PROGCPU) $(PROGCUDA)
 
+cleanobj:
+	rm -f $(OBJ)
--- matrix.cu
+++ matrix.cu
@@ -15,25 +15,27 @@
 #ifndef SIZE
 #define SIZE 1024
 #endif
-#define MEM_SIZE SIZE * SIZE
 
 #define MAX_VALUE 100 // 行列の各要素の最大値。実際の最大値はMAX_VALUE-1
 
 #ifdef __NVCC__
-#ifndef THREAD
-#define THREAD 16
-#endif
-#define BLOCK (SIZE / THREAD)
+  #ifndef THREAD
+  #define THREAD 16
+  #endif
 #endif
 
 #ifndef __NVCC__
-#ifdef __linux__
-typedef __clockid_t clockid_t;
-#endif
+  #ifdef __linux__
+  typedef __clockid_t clockid_t;
+  #endif
 extern int clock_gettime(clockid_t clk_id, struct timespec *tp);
+#else
+int block_num  = SIZE / THREAD; // この変数はmain()とCUDA版のto_squaring()で使用するが、to_squaring()の引数をCPU版とCUDA版で揃えたいためグローバルで宣言
 #endif
 
 typedef int matrix_t;
+
+int mem_size = sizeof(matrix_t) * SIZE * SIZE; // 行列一つに必要なメモリサイズ
 
 void print_matrix(matrix_t* matrix, int digit) {
     for (int i = 0; i < SIZE; i++) {
@@ -79,7 +81,7 @@ void kernel(matrix_t* matrix, matrix_t* matrix_ans) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     int j = blockIdx.y * blockDim.y + threadIdx.y;
 
-    if (i > SIZE || j > SIZE) { return; }
+    if (i >= SIZE || j >= SIZE) { return; }
 
     for (int k = 0; k < SIZE; k++) {
         matrix_ans[i + SIZE * j] += matrix[i + SIZE * k] * matrix[k + SIZE * j];
@@ -90,17 +92,19 @@ static inline void to_squaring(matrix_t* matrix, matrix_t* matrix_ans) {
     matrix_t* d_matrix;
     matrix_t* d_matrix_ans;
 
-    cudaMalloc((void**)&d_matrix,     sizeof(matrix_t) * MEM_SIZE);
-    cudaMalloc((void**)&d_matrix_ans, sizeof(matrix_t) * MEM_SIZE);
-    cudaMemcpy(d_matrix,     matrix,     sizeof(matrix_t) * MEM_SIZE, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_matrix_ans, matrix_ans, sizeof(matrix_t) * MEM_SIZE, cudaMemcpyHostToDevice);
+    cudaMalloc((void**)&d_matrix,     mem_size);
+    cudaMalloc((void**)&d_matrix_ans, mem_size);
+    cudaMemcpy(d_matrix,     matrix,     mem_size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_matrix_ans, matrix_ans, mem_size, cudaMemcpyHostToDevice);
+
+    if (SIZE % THREAD != 0) { block_num++; }
 
     dim3 thread = dim3(THREAD, THREAD, 1);
-    dim3 block  = dim3(BLOCK,  BLOCK,  1);
+    dim3 block  = dim3(block_num,  block_num,  1);
     kernel<<<block, thread>>>(d_matrix, d_matrix_ans);
     cudaThreadSynchronize();
 
-    cudaMemcpy(matrix_ans, d_matrix_ans, sizeof(matrix_t) * MEM_SIZE, cudaMemcpyDeviceToHost);
+    cudaMemcpy(matrix_ans, d_matrix_ans, mem_size, cudaMemcpyDeviceToHost);
     cudaFree(d_matrix);
     cudaFree(d_matrix_ans);
 }
@@ -127,8 +131,8 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    matrix_t* matrix     = (matrix_t*)malloc(sizeof(matrix_t) * MEM_SIZE);
-    matrix_t* matrix_ans = (matrix_t*)malloc(sizeof(matrix_t) * MEM_SIZE);
+    matrix_t* matrix     = (matrix_t*)malloc(mem_size);
+    matrix_t* matrix_ans = (matrix_t*)malloc(mem_size);
 
     init_matrix(matrix, matrix_ans);
 
@@ -143,21 +147,30 @@ int main(int argc, char* argv[]) {
 
     double end = get_time();
 
+    free(matrix);
+
     if (do_print) {
         printf("2乗後の行列\n");
         print_matrix(matrix_ans, get_digit(MAX_VALUE) * 2);
     }
 
-    free(matrix);
+    int top = matrix_ans[0];
+    int tail = matrix_ans[(SIZE - 1) * SIZE + (SIZE - 1)];
     free(matrix_ans);
 
     #ifdef __NVCC__
-    printf("Use CUDA: THREAD=%d, BLOCK=%d\n", THREAD, BLOCK);
+    printf("Use CUDA: THREAD=%d, BLOCK=%d\n", THREAD, block_num);
     #else
     printf("Use CPU only\n");
     #endif
     printf("Matrix size: %d * %d\n", SIZE, SIZE);
-    printf("Processing time: %lf sec\n", end - begin);
+    printf("matrix_ans[0][0]       = %d\n", top);
+    printf("matrix_ans[SIZE][SIZE] = %d\n", tail);
+    printf("\nProcessing time: %lf sec\n", end - begin);
 
+    if (top <= 0 || tail <= 0) {
+        fprintf(stderr, "error: calculation fails\n");
+        exit(1);
+    }
     return 0;
 }
